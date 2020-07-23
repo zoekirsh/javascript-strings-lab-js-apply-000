be065dd044c6fade14d5b917a977ea02695a60e4
Zoe Kirsh <zoekirsh@gmail.com>
2020-07-23 20:39:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello,";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = greeting + specialGuest + "!";
 
 var topic = "space";
 
